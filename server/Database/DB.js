b2ebd77c30e8dca3e6dbf6a5b25710f4b150f519
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-03-27 21:50:36 +0530

    created and connected database

---
+++ server/Database/DB.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+// create a .env file with the help of .env.sample 
+
+const USERNAME = process.env.DB_USERNAME
+const PASSWORD = process.env.DB_PASSWORD
+
+const connection = () => {
+
+    const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@mernstackcluster.syg0ylj.mongodb.net/?retryWrites=true&w=majority`;
+
+    // this is used to connect mongodb // old parser will be depriciated we will soon use new parser
+
+    mongoose.connect(MONGODB_URI , { useNewUrlParser : true });
+
+    mongoose.connection.on('connected',()=>{
+        console.log("database connected succefully")
+    })
+
+    mongoose.connection.on("disconnected",()=>{
+        console.log("database disconnected")
+    })
+
+    mongoose.connection.on('error',()=>{
+        console.log("error while connecting with database",error.message)
+    })
+}
+
+export default connection;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
-
+import connection from "./Database/DB.js";
 
 const app = express();
 
 const PORT = 8000;
+
+connection();
 
 app.listen(PORT,()=> console.log(`Server Initialised on port ${PORT}`))
