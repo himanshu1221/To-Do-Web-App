01e875bc43397d9d1267e0656343a7eba602250f
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-03-28 13:34:29 +0530

    created store.js

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,19 +1,21 @@
-import { createStore,combineReducers } from "redux"
-
+import { createStore,combineReducers,applyMiddleware } from "redux"
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({
     todos: todosReducer
 });
+
+const middleware = [thunk];
+
 const store = createStore(
 
     //we can perform action items with the help of reducers we use combine reducer to combine multiple task
     //with the help of redux devtools extension we can connect with dev tools extension in the browser to store the data 
-    //we are using thunk as a middle  ware 
+    //we are using thunk as a middle ware 
 
     reducer,
-    composeWithDevTools()
-
-
-
+    composeWithDevTools(applyMiddleware(...middleware))
 )
+
+export default store;
