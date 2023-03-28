6a1d0ddf225900bcd433cb42267a3f7fadff07c2
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-03-28 12:36:44 +0530

    added thunk dependencies to make it work as a middleware

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,9 +1,18 @@
 import { createStore,combineReducers } from "redux"
 
-const reducer = combineReducers();
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({
+    todos: todosReducer
+});
 const store = createStore(
 
     //we can perform action items with the help of reducers we use combine reducer to combine multiple task
+    //with the help of redux devtools extension we can connect with dev tools extension in the browser to store the data 
+    //we are using thunk as a middle  ware 
+
+    reducer,
+    composeWithDevTools()
 
 
 
