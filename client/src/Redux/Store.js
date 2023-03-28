9004fdec1debc38ea57d3f1dda30020868309f10
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-03-28 12:31:30 +0530

    added redux devtools extension

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux"
+import { createStore,combineReducers } from "redux"
 
-
+const reducer = combineReducers();
 const store = createStore(
-    
+
+    //we can perform action items with the help of reducers we use combine reducer to combine multiple task
+
+
+
 )
