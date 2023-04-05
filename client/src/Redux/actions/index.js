227721e7caa150db957885071b78a41f88daf7c1
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-05 15:07:59 +0530

    deploying on vercel

--- client/src/Redux/actions/index.js
+++ client/src/Redux/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { ADDNEW_TODO, GETALL_TODO,TOGGLE_TODO,UPDATE_TODO,DELETE_TODO,TOGGLE_TAB } from "./type";
-const API_URL = "http://localhost:8000";
+const API_URL = "https://to-do-web-app-two.vercel.app/";
 
 export const addNewTodo = (data) => async(dispatch) =>{
 
